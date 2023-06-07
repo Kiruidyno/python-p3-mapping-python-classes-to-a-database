8334b961072782efcb8df58a3fe9012f61391dec
Kiruidyno <kirui472@gmail.com>
2023-06-07 10:26:21 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -4,5 +4,77 @@ from config import CONN, CURSOR
 from song import Song
 
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+class Song:
+    all = []  # Class variable to store all song instances
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS songs
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        cls.all = [cls(*row) for row in rows]
+        return cls.all
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
+
+
+
+def reset_database():
+    Song.drop_table()
+    Song.create_table()
+    Song.create("Hello", "25")
+    Song.create("99 Problems", "The Black Album")
+
+
+reset_database()
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,52 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    all = []  # List to store all song instances
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+
+        # Fetch the last inserted row id
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+        return song
+
+    @classmethod
+    def all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+        all_songs = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all_songs]
+        return cls.all
